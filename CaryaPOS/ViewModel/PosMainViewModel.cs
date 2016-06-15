ad395a5df1bfef4e2712707be451dfdbe96041a6
ft115637850 <gentleman000000@hotmail.com>
2016-06-15 23:16:14 +0800

    Implement goods list

--- CaryaPOS/ViewModel/PosMainViewModel.cs
+++ CaryaPOS/ViewModel/PosMainViewModel.cs
@@ -10,9 +10,14 @@ namespace CaryaPOS.ViewModel
     class PosMainViewModel : ViewModelBase
     {
         public List<CategoryViewModel> GoodsCategoriesInfo { get; set; }
+        public List<GoodsViewModel> Goods { get; set; }
         public PosMainViewModel(List<CategoryViewModel> goodsCategories)
         {
             this.GoodsCategoriesInfo = goodsCategories;
+            if (goodsCategories.Count > 0)
+            {
+                this.Goods = goodsCategories[0].GoodsList;
+            }
         }
     }
 }
