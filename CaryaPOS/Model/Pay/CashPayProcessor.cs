bcff49e0e715b844be8c3605ddf88e1238a868ce
ft115637850 <gentleman000000@hotmail.com>
2016-10-16 10:18:52 +0800

    Add CashPayProcessor.cs

---
+++ CaryaPOS/Model/Pay/CashPayProcessor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaryaPOS.Model
+{
+    class CashPayProcessor : PayProcessor
+    {
+        private const int payTypeID = 1;
+        private const string payName = "Cash";  //TO DO: Localization
+
+        public CashPayProcessor(string sheetID)
+            : base(sheetID)
+        {
+
+        }
+
+        public override decimal GetPayItemValue()
+        {
+            return this.salesDBDao.GetPayItemValue(this.sheetID, CashPayProcessor.payTypeID);
+        }
+
+        public override void UpdatePayRecord()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
